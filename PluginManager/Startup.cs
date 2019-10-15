32b8649ecbac84d5152339be436110e3676f13b1
ngouz <ng_dieubeni@yahoo.fr>
2019-10-15 11:11:50 +0200

    Plugin Manager WebApplication

---
+++ PluginManager/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ExtCore.WebApplication.Extensions;
+
+namespace PluginManager
+{
+    public class Startup
+
+    {
+        private string extensionsPath;
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddExtCore(this.extensionsPath);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.UseExtCore();
+            applicationBuilder.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello Marcel!");
+            });
+        }
+    }
+}
